65f5211e136cc1739a2495f0faedf845a7ef1355
Caubert Castro <caubert.castro@gmail.com>
2020-04-26 02:52:07 -0300

    Resolução do terceiro desafio em nodeJS

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,25 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const income = transactions.reduce((acc, transaction) => {
+      const value = +transaction.value; // convertendo o valor que vem do banco como string
+      return transaction.type !== 'income' ? acc : acc + value;
+    }, 0);
+
+    const outcome = transactions.reduce((acc, transaction) => {
+      const value = +transaction.value; // convertendo o valor que vem do banco como string
+      return transaction.type !== 'outcome' ? acc : acc + value;
+    }, 0);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,52 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+    const categoryRepository = getRepository(Category);
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value)
+      throw new AppError('The input value is greater than total value!');
+
+    let transactionCategory = await categoryRepository.findOne({
+      where: { category },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({ title: category });
+      await categoryRepository.save(transactionCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
